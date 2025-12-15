f5df4ae7d193956c4694c62588854a4bc8c5d9ca
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2025-12-15 09:33:27 +0100

    Fix variables in mails

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import base64
+
 from odoo import models
 
 
@@ -15,5 +17,30 @@ class SaleOrder(models.Model):
         if template:
             for order in self:
                 if order.partner_id and order.partner_id.email:
-                    template.send_mail(order.id, force_send=True, raise_exception=False)
+                    attachment_id = False
+                    report = self.env.ref("sale.action_report_saleorder", raise_if_not_found=False)
+                    if report:
+                        pdf_content, _content_type = report._render_qweb_pdf([order.id])
+                        attachment = self.env["ir.attachment"].create(
+                            {
+                                "name": f"{order.name}.pdf",
+                                "type": "binary",
+                                "datas": base64.b64encode(pdf_content),
+                                "mimetype": "application/pdf",
+                                "res_model": order._name,
+                                "res_id": order.id,
+                            }
+                        )
+                        attachment_id = attachment.id
+
+                    email_values = None
+                    if attachment_id:
+                        email_values = {"attachment_ids": [(4, attachment_id)]}
+
+                    template.send_mail(
+                        order.id,
+                        force_send=True,
+                        raise_exception=False,
+                        email_values=email_values,
+                    )
         return res
